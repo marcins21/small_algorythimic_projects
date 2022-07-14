1977d8ec1952776fd185e21209af99a526ba8476
marcins21 <62626012+marcins21@users.noreply.github.com>
2022-07-14 17:02:56 +0200

    Update q1
    Wykonanie Kolejnych 'interview questions'

--- q1.py
+++ q1.py
@@ -64,7 +64,7 @@ def is_palindrome(number:int):
     #print(is_palindrome(123321))
 
 
-# (6.1) Checking if  number is  palindrome using list/string
+# (6.1) Checking if  expression is  palindrome using list/string
 def is_palindrome_list(number):
     number = list(number)
     result = []
@@ -135,4 +135,109 @@ def is_Perfect(number):
     else:
         return False
 #   print(is_Perfect(6))
+
+# (12) Power Function iterative
+def number_power(number:float, exp:int):
+    temp = 1
+    for i in range(exp):
+        temp *= number
+    return temp
+#   print(number_power(2,3))
+
+# (13) Power Function recursive
+def number_power_recursive(number:float, exp:int):
+    if exp == 0:
+        return 1
+    return number * number_power_recursive(number,exp-1)
+#   print(number_power_recursive(2,3))
+
+# (14) Finding average of given numbers
+def average_number(numbers:list):
+    #leng = len(numbers)
+    #return sum(numbers)/leng
+    temp = 0
+    lengh = len(numbers)
+    for i in range(len(numbers)):
+        temp += numbers[i]
+    return temp/lengh
+#   print(average_number([1,2,3]))
+
+#-----------------------https://quescol.com/interview-preparation/python-coding-question---------------------------
+
+# Q-17 Find Factorial of a number Iterative method
+def factorial(number:int)-> int:
+    temp = 1
+    for i in range(number):
+        temp *= number-i
+    return temp
+#   print(factorial(4))
+
+# Q-18 Find Factorial Recursive method
+def factorial_recursive(number:int):
+    if number < 2:
+        return 1
+    return number * factorial_recursive(number-1)
+#   print(factorial_recursive(4))
+
+# Q-19 Check if number is odd or even
+def odd_or_even(number:int):
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+# Q-20 Print N first prime numbers
+def n_primeNumbers(number:int):
+    counter = 0
+    result = []
+
+    #meh ;( To Continue
+    temp_low = 2
+    temp_high = 1500
+    ################
+
+    for i in range(temp_low,temp_high):
+        for j in range(temp_low,i+1):
+            if i == j:
+                result.append(i)
+            if i % j == 0:
+                break
+
+    if len(result) >= number:
+        for i in range(number):
+            print(i+1,":",result[i])
+# n_primeNumbers(100)
+
+# Q-21 Print N Prime Numbers in a given range
+def prime_numbers_in_range(lower_range:int,higher_range:int):
+    for i in range(lower_range,higher_range):
+        for j in range(2,i+1):
+            if i == j:
+                print(i)
+            if i % j == 0:
+                break
+#   prime_numbers_in_range(190,200)
+
+# Q-22 Done!
+# Q-23 Done!
+# Q-24 Power of a number without POW method (using FOR LOOP)
+def power_of_number(number,exp:int)->float:
+    temp = 1
+    for i in range(exp):
+        temp *= number
+    return temp
+#   print(power_of_number(2,3))
+
+# Q-25 Power a number without POW method (using WHILE LOOP)
+def power_of_number_while(number,exp:int):
+    temp = 1
+    counter = 0
+    while counter != exp:
+        temp *= number
+        counter +=1
+    return temp
+#   print(power_of_number_while(2,3))
+
+
+
 
